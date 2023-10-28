47d95f1406300a3dc50d442b48d72d23ffd56198
LearnWithTripurari <learn.with.tripurari@gmail.com>
2023-10-28 21:45:05 +0530

    refactor(Header): Initial code refactor

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -4,20 +4,13 @@ import './Header.scss';
 function Header() {
     return (
         <div className="header">
-
             <div className="left-section">
                 <span className="material-symbols-outlined color-white menu-icon">
                   menu
                 </span>
-                <span className="material-symbols-outlined color-white home-icon">
-                  home
-                </span>
 
             </div>
 
-            <div>
-
-            </div>
         </div>
 
     );
