4b64295b30064601e7a6b1523f0ec217b7d87378
LearnWithTripurari <learn.with.tripurari@gmail.com>
2023-11-03 08:38:48 +0530

    feat(client): Designed the Task Board - Initial Design has been done

--- client/src/Components/Main/Main.jsx
+++ client/src/Components/Main/Main.jsx
@@ -2,8 +2,48 @@ import { h } from 'preact';
 import './Main.scss';
 
 function Main() {
+
+    function onCreateTask() {
+        const createTaskInput = document.querySelector('.create-task-input > input');
+
+        if ( createTaskInput.style.display === 'block')
+            createTaskInput.style.display = 'none';
+        else
+            createTaskInput.style.display = 'block'
+    }
+
+    function onEnter(e) {
+        if (e.key === 'Enter') {
+            console.log(e.target.value);
+        }
+    }
+
     return (
         <main className="main-content">
+
+            <div className="board">
+                <div className="box">
+                    <div className="title">TO DO</div>
+                    <div className="create-task">
+                        <a className="link-button" onClick={onCreateTask}>
+                            <span className="material-symbols-outlined">add</span>
+                            Create task
+                        </a>
+                        <div className="create-task-input">
+                            <input type="text" placeholder="What needs to be done?" onKeyUp={onEnter}/>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div className="box">
+                    <div className="title">IN PROGRESS</div>
+                </div>
+
+                <div className="box">
+                    <div className="title">DONE</div>
+                </div>
+            </div>
 
         </main>
     );
